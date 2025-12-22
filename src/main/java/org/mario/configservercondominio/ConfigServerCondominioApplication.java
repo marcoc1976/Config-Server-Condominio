1afafe94dfc40bcadd6f83e88b147c8c23f85339
mario.cocozza <mariococozza76@gmail.com>
2025-12-22 19:38:49 +0100

    initial revision

---
+++ src/main/java/org/mario/configservercondominio/ConfigServerCondominioApplication.java
@@ -0,0 +1,13 @@
+package org.mario.configservercondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerCondominioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerCondominioApplication.class, args);
+    }
+
+}
